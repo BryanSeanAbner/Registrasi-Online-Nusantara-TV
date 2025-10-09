1c3545b3aa902f467be028b465543cc06ecc23d3
rizk1 <srizki4455@gmail.com>
2025-10-09 17:07:00 +0700

    update dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Widgets\OverviewStats;
+use App\Filament\Widgets\RecentRegistrations;
+use App\Filament\Widgets\RecentScans;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -12,7 +15,6 @@ use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\View\PanelsRenderHook;
 use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -49,7 +51,9 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
                 AccountWidget::class,
-                FilamentInfoWidget::class,
+                OverviewStats::class,
+                RecentRegistrations::class,
+                RecentScans::class,
             ])
             ->middleware([
                 EncryptCookies::class,
@@ -69,3 +73,4 @@ class AdminPanelProvider extends PanelProvider
             ]);
     }
 }
+
