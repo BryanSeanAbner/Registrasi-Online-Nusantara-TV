fd7c2301a90ba3f229533533d3c364e575a22149
pricilladwi <pricilladwi24@gmail.com>
2025-10-10 14:29:56 +0700

    perubahan nama title halaman

--- resources/views/public/shared/layout.blade.php
+++ resources/views/public/shared/layout.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>{{ config('app.name', 'Askara Simple') }}</title>
+  <title>{{ config('app.name', 'Registrasi') }}{{ isset($event) ? ' - ' . $event->title : '' }}</title>
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   @stack('styles')
 </head>
