c3748d767f1b6544718c940ed371f3c9f7fd5f4e
BryanSeanAbner <bryansean425@gmail.com>
2025-10-10 14:04:15 +0700

    commit

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\Registration;
 
 class EventController extends Controller {
 
@@ -15,5 +16,35 @@ class EventController extends Controller {
     public function show(string $slug) {
         $event = Event::whereSlug($slug)->where('is_published', true)->firstOrFail();
         return view('public.events.show', compact('event')); 
+    }
+
+    public function participants(string $slug)
+    {
+        $event = Event::whereSlug($slug)
+            ->where('is_published', true)
+            ->firstOrFail();
+
+        $registrations = Registration::with(['values.field'])
+            ->where('event_id', $event->id)
+            ->orderByDesc('created_at')
+            ->get();
+        $total = $registrations->count();
+
+        return view('public.register.participant', compact('event','registrations','total'));
+    }
+
+    public function participantsLatest()
+    {
+        $event = Event::where('is_published', true)
+            ->orderByDesc('starts_at')
+            ->firstOrFail();
+
+        $registrations = Registration::with(['values.field'])
+            ->where('event_id', $event->id)
+            ->orderByDesc('created_at')
+            ->get();
+        $total = $registrations->count();
+
+        return view('public.register.participant', compact('event','registrations','total'));
     }
 }
