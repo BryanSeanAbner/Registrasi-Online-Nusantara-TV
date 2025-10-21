bd21df7870e6fe61963d5e60849a854990e69d45
rizk1 <srizki4455@gmail.com>
2025-10-21 15:57:20 +0700

    fix login page

--- app/Filament/Auth/Login.php
+++ app/Filament/Auth/Login.php
@@ -4,27 +4,29 @@ namespace App\Filament\Auth;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Auth\Pages\Login as BaseLogin;
-use Filament\Schemas\Components\Form as SchemaForm;
 use Filament\Schemas\Schema;
+use Filament\Forms\Components\Checkbox;
 
 class Login extends BaseLogin
 {
     public function form(Schema $schema): Schema
     {
         return $schema->components([
-            SchemaForm::make()->schema([
-                TextInput::make('login')
-                    ->label('Email atau Username')
-                    ->required()
-                    ->autofocus()
-                    ->autocomplete('username'),
+            TextInput::make('email')
+                ->label('Email atau Username')
+                ->required()
+                ->autofocus()
+                ->autocomplete('username'),
 
-                TextInput::make('password')
-                    ->label('Password')
-                    ->password()
-                    ->revealable()
-                    ->required(),
-            ]),
+            TextInput::make('password')
+                ->label('Password')
+                ->password()
+                ->revealable()
+                ->required()
+                ->extraAttributes(['class' => 'mb-4']),
+
+            Checkbox::make('remember')
+                ->label(__('filament-panels::auth/pages/login.form.remember.label')),
         ]);
     }
 
@@ -36,13 +38,13 @@ class Login extends BaseLogin
      */
     protected function getCredentialsFromFormData(array $data): array
     {
-        $login = (string) ($data['login'] ?? '');
+        $value = (string) ($data['email'] ?? '');
         $password = (string) ($data['password'] ?? '');
 
-        $field = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+        $field = filter_var($value, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
 
         return [
-            $field => $login,
+            $field => $value,
             'password' => $password,
         ];
     }
