b45a024037918f96d88dc9e2a8306b74650e97dc
rizk1 <srizki4455@gmail.com>
2025-10-30 15:34:01 +0700

    fix slug update after change title event

--- app/Filament/Resources/Event/Schemas/EventForm.php
+++ app/Filament/Resources/Event/Schemas/EventForm.php
@@ -33,20 +33,18 @@ class EventForm
                                 ->required()
                                 ->live(onBlur: true)
                                 ->afterStateUpdated(function (Set $set, Get $get, $state) {
-                                    if ($get('id')) {
-                                        return;
-                                    }
-
                                     $base = Str::slug($state ?? '');
                                     if ($base === '') {
                                         $set('slug', '');
                                         return;
                                     }
 
+                                    $id = $get('id');
                                     $slug = $base;
                                     $i = 2;
                                     while (
                                         Event::query()
+                                            ->when($id, fn ($q) => $q->where('id', '!=', $id))
                                             ->where('slug', $slug)
                                             ->exists()
                                     ) {
