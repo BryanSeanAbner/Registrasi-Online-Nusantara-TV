ed02dcf3f21d43ef211ed2fa0b415906efbf80eb
rizk1 <srizki4455@gmail.com>
2025-11-07 10:53:05 +0700

    update redirect edit formfield

--- app/Filament/Resources/FormField/Pages/EditFormField.php
+++ app/Filament/Resources/FormField/Pages/EditFormField.php
@@ -8,5 +8,10 @@ use Filament\Resources\Pages\EditRecord;
 class EditFormField extends EditRecord
 {
     protected static string $resource = FormFieldResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
 
